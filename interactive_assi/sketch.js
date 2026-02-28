c2114c17956b71c567cd786b6b62d30be8e942b0
timtruong2009-blip <tim.truong.2009@gmail.com>
2026-02-27 18:34:30 -0600

    update inter

--- interactive_assi/sketch.js
+++ interactive_assi/sketch.js
@@ -1,6 +1,6 @@
-// Project Title
-// Your Name
-// Date
+// Interactive scene Assignment
+// Tim Truong
+// Feb something
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
